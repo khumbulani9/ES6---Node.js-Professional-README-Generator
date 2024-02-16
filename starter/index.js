588b19c974c0270f2108160fe5783c060c2c8d54
khumbulani9 <mduduzi1234gmail.com>
2024-02-16 20:36:21 +0000

    My first commit of updating the index.js file and the generateMarkdown.js file

--- starter/index.js
+++ starter/index.js
@@ -1,19 +1,80 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
+const inquirer = reuire("inquirer");
+const fs = reuire('fs');
+const util = require('util')
+
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = [{
+  type: "input",
+  message: "What is the Title of the project",
+  name: "Title" 
+}, {
+  type: "input",
+  message: "What is the project about? and a give a detailed description of your contents",
+  name: "Description"
+}, {
+  type: "input",
+  message: "Table of contents",
+  name: "Table of contents"
+}, {
+  type: "input",
+  message: "what does the user need to install to run this app",
+  name: "Installation"
+}, {
+  type: "input",
+  message:"How is the App used to give instructions",
+  name:"Usage"
+}, {
+type: "input",
+message: "What Licence is being used?",
+name: "License"
+}, {
+  type:"input",
+  message:"What commands are needed to test this app?",
+  name:"Tests"
+}, {
+  type:"input",
+  message:"Who contributed to the project",
+  name: "Contributing"
+}, {
+  type:"input"
+  message:"Contact information for inquiries.",
+  name:"Questions"
+}, {
+  type:"input",
+  message:"What is your Github name?",
+  name:"Username"
+}, {
+  type:"input",
+  message:"What is your email address?",
+  name: "Email"
+},
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+  fs.writeFile(fileName, data, function(err) {
+    console.log(fileName)
+    console.log(data)
+    if(err) {
+      return console.log(err)
+      } else {
+        console.log("success")
+    }
+  })
+  
 }
 
 // function to initialize program
 function init() {
+  inquirer.createPromptModule(questions)
+  .then(function(data) {
+    writeToFile("README.md", generatorMarkdown(data));
+    console.log(data)
+  })
 
 }
 
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  https://github.com/${data.Username}/${data.Title}
+  #Description
+  ${data.Description}
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  # Installation
+The following necessary dependencies must be installed to run the application.
+# Usage
+Inorder to use this app, ${data.Usage}
+# License
+This project is licensed under the ${data.license} license.
+![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+# Contributing
+contributors: ${data.contributing}
+# Tests
+The following is needed to the run tests: ${data.Tests}
+# Questions 
+If you have any questions about the repo, open an issue or contact ${data.Questions}
 
-`;
+`
 }
-
 module.exports = generateMarkdown;
